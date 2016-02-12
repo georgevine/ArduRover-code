157d9486a6680223a57f0a618ca93dd5747c8507
georgevine <george.h.vine.iv@gmail.com>
2016-02-11 22:25:29 -0800

    Added sensortest code
    A basic arduino sketch that allows the user to issue a command over
    bluetooth that tells the arduino to read and return the distance from a
    distance sensor and blink the embedded LED

---
+++ sensortest_onePing/sensortest_onePing.ino
@@ -0,0 +1,32 @@
+
+#include <NewPing.h>
+
+#define TPIN   12 //pin we use to send our ping command
+#define EPIN   11 //pin we use to get data back from the sensor
+#define BLINK_PIN 13 //this guy will blink
+
+NewPing sonar(TPIN, EPIN, 400); // 400 cms will be our max distance
+
+char blueToothVal;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  if(Serial.available()){
+    blueToothVal=Serial.read();
+  }
+
+  if(blueToothVal == 'r'){
+    digitalWrite(13, HIGH);
+    Serial.print("Ping: ");
+    Serial.print(sonar.ping_cm());
+    Serial.println("cm");
+    delay(1000);
+    digitalWrite(13, LOW);
+    blueToothVal = 'x';
+  }
+}
+
