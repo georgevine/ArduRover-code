57c926a4e443d6b1bb80dd3be3f5c3c8bdcd4f6e
George Vine <george.h.vine.iv@gmail.com>
2016-06-21 20:08:38 -0700

    Made final commit to auto sketch

--- first_auto/first_auto.ino
+++ first_auto/first_auto.ino
@@ -2,8 +2,12 @@
 
 //low = back, high = forward
 
-#define TPIN   4 //pin we use to send our ping command
-#define EPIN   5 //pin we use to get data back from the sensor
+#define TPIN_M   4 //pin we use to send our ping command
+#define EPIN_M  5 //pin we use to get data back from the sensor
+#define TPIN_L A4
+#define EPIN_L A5
+#define TPIN_R A2
+#define EPIN_R A3
 #define rightInitPin 13
 #define rightBrakePin 8
 #define leftInitPin 12
@@ -19,8 +23,14 @@ int leftMotorPower;
 int leftMotorDir;
 int rightMotorDir;
 int distance = 1000;
+int distance_m = 1000;
+int distance_l = 1000;
+int distance_r = 1000;
+bool aut = false;
+NewPing sonar_m(TPIN_M, EPIN_M, 400);
+NewPing sonar_l(TPIN_L, EPIN_L, 400);
+NewPing sonar_r(TPIN_R, EPIN_R, 400);
 
-NewPing sonar(TPIN, EPIN, 400);
 
 
 
@@ -46,35 +56,78 @@ void setup() {
 }
 
 void loop(){
-  distance = sonar.ping_cm();
-  if(distance < 30){
-    brake();
-    back(200);
-    delay(500);
-    scan();
-  }
-  else if(distance < 75){
-    stopAll();
-    int rand = (int)(random(2));
-    if(rand == 1){
-      stopAll();
-      turnRight(100);
-      delay(random(150,400));
-      stopAll();
-    }
-    else{
-      stopAll();
-      turnLeft(100);
-      delay(random(250,400));
-      stopAll();
-    }
-  }
-  else{
-    forward(200);
+  if(Serial.available()){
+     input = Serial.readString();
   }
 
-  
-  
+  if(input.indexOf("A") > -1){
+    aut= true;
+  }
+  else if(input.indexOf("M") > -1){
+    aut = false;
+  }
+
+  if(!aut){
+    if(input.indexOf("<") > -1){
+    Serial.print("here");
+    input.remove(0,1);
+    leftMotorPower = input.toInt();
+    input = " ";
+    }
+    else if(input.indexOf(">") > -1){
+      input.remove(0,1);
+      rightMotorPower = input.toInt();
+      input = " ";
+    }
+    else if(input == "lr"){
+      leftMotorDir = LOW;
+    }
+    else if(input == "lf"){
+      leftMotorDir = HIGH;
+    }
+    else if(input == "rr"){
+      rightMotorDir = LOW;
+    }
+    else if(input == "rf"){
+      rightMotorDir = HIGH;
+    }
+    spinLeftMotor(leftMotorPower, leftMotorDir);
+    spinRightMotor(rightMotorPower, rightMotorDir);
+    delay(10);
+  }
+
+  else{
+    distance_l = sonar_l.ping_cm();
+    if(distance_l == 0){
+      distance_l = 400;
+    }
+    delay(50);
+    distance_m = sonar_m.ping_cm();
+    if(distance_m == 0){
+      distance_m = 400;
+    }
+    delay(50);
+    distance_r = sonar_r.ping_cm();
+    if(distance_r == 0){
+      distance_r = 400;
+    }
+    delay(50);
+    if(distance_r < 8 || distance_m < 8 || distance_l < 8){
+      brake();
+      reverse(200);
+      delay(700);
+      brake();
+      scan();
+    } else {
+      if(distance_m < 75){
+        turnTowardGreatestDistance();
+      }
+      else{
+        forward(120);
+        delay(100);
+      }
+    }
+  }
 }
 
 
@@ -131,20 +184,76 @@ void brake(){
 }
 
 void scan(){
-  int rightDistance;
-  int leftDistance;
+  int rightAverage;
+  int leftAverage;
   turnRight(200);
-  delay(700);
+  delay(300);
   brake();
-  rightDistance = sonar.ping_cm();
+  distance_m = sonar_m.ping_cm();
+  distance_l = sonar_l.ping_cm();
+  distance_r = sonar_r.ping_cm();
+  if(distance_r == 0){
+    distance_r = 400;
+  }
+  if(distance_m == 0){
+    distance_m = 400;
+  }
+  if(distance_l == 0){
+    distance_l = 400;
+  }
+  rightAverage = (distance_m + distance_l + distance_r)/3;
+  
   turnLeft(200);
-  delay(700);
+  delay(300);
   brake();
-  leftDistance = sonar.ping_cm();
+  distance_m = sonar_m.ping_cm();
+  distance_l = sonar_l.ping_cm();
+  distance_r = sonar_r.ping_cm();
+  if(distance_r == 0){
+    distance_r = 400;
+  }
+  if(distance_m == 0){
+    distance_m = 400;
+  }
+  if(distance_l == 0){
+    distance_l = 400;
+  }
+  leftAverage = (distance_m + distance_l + distance_r)/3;
 
-  if(rightDistance > leftDistance){
-    turnRight(1400);
-    delay(200);
+  if(rightAverage > leftAverage){
+    turnRight(200);
+    delay(600);
+  }
+}
+
+
+void turnTowardGreatestDistance(){
+  int large = 0;
+  String largestDist = " ";
+  if(distance_m > large){
+    large = distance_m;
+    largestDist = "m";
+  }
+  if(distance_r > large){
+    large = distance_r;
+    largestDist = "r";
+  }
+  if(distance_l > large){
+    large = distance_l;
+    largestDist = "l";
+  }
+
+  if(largestDist.equals("l")){
+    stopAll();
+    turnLeft(200);
+    delay(100);
+    stopAll();
+  }
+  else if(largestDist.equals("r")){
+    stopAll();
+    turnRight(200);
+    delay(100);
+    stopAll();
   }
 }
 
